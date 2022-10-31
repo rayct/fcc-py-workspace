0e80a543541f08060993206d1e0ab649b28b5510
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-10-31 23:45:19 +0000

    First Commit

---
+++ fcc-python-workspace.py
@@ -0,0 +1,25 @@
+
+# Strings and Lists
+
+# words = 'His e-mail is q-lar@freecodecamp.org'
+# pieces = words.split()
+# parts = pieces[3].split('-')
+# n = parts[1]
+# print(n)
+
+
+# Python Dictionaries
+
+# dict = {"Fri": 20, "Thu": 6, "Sat": 1}
+# dict["Thu"] = 13
+# dict["Sat"] = 2
+# dict["Sun"] = 9
+# print(dict)
+
+
+
+# Dictionaries: Common Applications
+# counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
+# print(counts.get('kris', 0))
+
+# TEST
