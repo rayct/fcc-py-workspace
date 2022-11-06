f017b267ada4b6207450704dfc4b498ad5296f4a
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-11-06 01:47:29 +0000

    Networking Build a Web Browser using Sockets

--- fcc-python-workspace.py
+++ fcc-python-workspace.py
@@ -119,27 +119,36 @@
 # Networking with Python
 
 # Networking: Write a Web Browser
-import socket
+# import socket
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt  HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+# mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# mysock.connect(('data.pr4e.org', 80))
+# cmd = 'GET http://data.pr4e.org/romeo.txt  HTTP/1.0\r\n\r\n'.encode()
+# mysock.send(cmd)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
-mysock.close()
-print(mysock)
+# while True:
+#     data = mysock.recv(512)
+#     if len(data) < 1:
+#         break
+#     print(data.decode(),end='')
+# mysock.close()
+# print(mysock)
 
 # https://data.pr4e.org/romeo.txt
 # https://www.ellatronix.com/notes.txt
 
 
+# Networking: Text Processing
+# UTF-8
+# Find the Character set value:
+# print(ord('%'))
 
 
+# Networking: Using urllib in Python
+import urllib.request
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
 
 
 
